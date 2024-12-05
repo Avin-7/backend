53dc2788fb4bd7c5f3a6f74174edbfc553af9903
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-12-05 11:50:22 +0530

    add utils code

--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR WHILE INITIALIZING APP !!");
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MONGODB CONNECTION FAILED"));
