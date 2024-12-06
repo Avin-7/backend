93dc768bb981caafa5370ed7b5b9a46e24bdb655
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-12-06 11:48:08 +0530

    add router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
+
 
 connectDB()
   .then(() => {
@@ -14,7 +16,7 @@ connectDB()
       throw error;
     });
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port : ${process.env.PORT}`);
+      console.log(`Server is running at port : ${process.env.PORT} `);
     });
   })
-  .catch((err) => console.log("MONGODB CONNECTION FAILED"));
+  .catch((err) => console.log("MONGODB CONNECTION FAILED", err));
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"; //file system provided by Node js
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload on cloudinary
+    const res = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File saved successfully !!", res.url);
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //removes the locally saves file since uploading on cloudinary failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
