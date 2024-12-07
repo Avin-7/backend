71140f2ca476c3bb5c81f747cb2057e4c6110ff2
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-12-07 10:00:29 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,71 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import ApiError from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  // res.status(200).json({
+  //   message: "ok",
+  // });
+
+  // user details
+  const { username, email, password, fullname } = req.body;
+  console.log("email : ", email);
+
+  // validate data feilds
+  if (
+    [username, email, password, fullname].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // check if user exists
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }], // $or: is used to send 2 values for querying, this stt will return a user with whom either the email or username matches.
   });
+  if (existedUser) {
+    throw new ApiError(409, "User with this username and email already exists");
+  }
+
+  //check avatar and cover images
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImgLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  //upload them to cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImgLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file required");
+  }
+
+  const user = await User.create({
+    username: username.toLowerCase(),
+    email,
+    password,
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  // here we are removing password and refreshToken from our response. since it is a critical data better not to share throughout the application.
+
+  // -password means that passowrd is deselected from the object. for deselection - (minus) symbol is used.
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registred successfully"));
 });
 export { registerUser };
